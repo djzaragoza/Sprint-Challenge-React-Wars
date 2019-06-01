84c42e4ddca672e9cb941cb230378c2cd6cff66a
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-01 10:22:26 -0700

    finished character.js file

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -20,8 +20,26 @@ const Character = props => {
                 <p><strong>Eye Color:</strong></p>
                 <p>{props.character.eye_color}</p>
             </div>
-            
+            <div className="info">
+                <p><strong>Skin Tone:</strong></p>
+                <p>{props.character.skin_color}</p>
+            </div>
+            <div className="info">
+                <p><strong>Hair Color:</strong></p>
+                <p>{props.character.hair_color}</p>
+            </div>
+            <div className="info">
+                <p><strong>Weight: </strong></p>
+                <p>{props.character.mass} Kilos</p>
+            </div>
+            <div className="info">
+                <p><strong>Height: </strong></p>
+                <p>{props.character.height}</p>
+            </div>
         </div>
+    </div>
 
     )
 } 
+
+export default Character;
